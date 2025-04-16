c03a0d9e37f95d8d4da139843b15b982c5b7f136
João Kuster <kusterzito@gmail.com>
2025-04-15 22:07:20 -0300

    feat(ui): hero section with new color pallet

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <header className="p-5 gap-5 flex items-center justify-between">
-            <h1 className="font-bold text-2xl">Solidari</h1>
-            <nav className="flex items-center gap-5">
-                <Link to="/doacoes">Doações</Link>
-                <Link to="/doar">Doar</Link>
-                <Link to="/pedir">Pedir</Link>
-                <Link to="/sobre">Sobre</Link>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="bg-blue-950 p-5 gap-5 flex items-center justify-between">
+      <h1 className="font-bold text-2xl text-neutral-200">Solidari</h1>
+      <nav className="flex text-neutral-300 items-center gap-5">
+        <Link to="/doacoes">Doações</Link>
+        <Link to="/doar">Doar</Link>
+        <Link to="/pedir">Pedir</Link>
+        <Link to="/sobre">Sobre</Link>
+      </nav>
+    </header>
+  );
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,27 +1,33 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 function Home() {
-    return (
-        <div
-            className="hero min-h-screen"
-            style={{
-                backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-            }}>
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-xl">
-                    <h1 className="mb-5 text-5xl font-bold">Solidari</h1>
-                    <p className="mb-5 text-xl text-neutral-content">
-                        Conectando doadores a quem mais precisa! <br />
-No Solidari, a gente faz a ponte da solidariedade acontecer. <br />
-Com empatia, simplicidade e amor.
-                    </p>
-                    <div className="flex justify-center gap-5">
-                        <button className="btn btn-accent">Quero doar</button>
-                        <button className="btn">Preciso de ajuda</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="pt-32 relative min-h-screen bg-linear-to-t from-sky-500 to-blue-950">
+      <div className="space-y-12 text-center m-auto max-w-xl">
+        <h1 className="text-5xl font-bold text-neutral-300">
+          Encontre ou receba o que procura
+        </h1>
+        <p className="text-xl text-neutral-300">
+          Conectando doadores a quem mais precisa! <br />
+        </p>
+        <form className="flex items-center items-stretch gap-x-2" action="">
+          <div className="relative flex-1">
+            <label className="sr-only" htmlFor="search">
+              Procurar algo de interesse
+            </label>
+            <input
+              type="text"
+              className="pl-10 border-2 border-transparent focus:border-orange-600/50 w-full bg-neutral-200 py-4 outline-none placeholder:text-neutral-400 rounded-md"
+              placeholder="Insira um móvel, tipo de agasalho, calçado"
+            />
+            <MagnifyingGlassIcon className="w-6 h-6 text-neutral-500 absolute left-2 inset-y-0 my-auto" />
+          </div>
+          <button className="hover:bg-orange-600 shadow-lg hover:shadow-none cursor-pointer rounded-md px-10 bg-secondary text-neutral-200 font-semibold">
+            Procurar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
